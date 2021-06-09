1e71ee58161e75122797dc600ba4844690dddfd6
Jon Brook <jonbrook89@gmail.com>
2021-06-09 18:16:22 +0100

    Updated the db models

--- models/index.js
+++ models/index.js
@@ -45,16 +45,68 @@ const Result = sequelize.define("result", {
   },
 });
 
+const User = sequelize.define("user", {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+const Pool = sequelize.define("pool", {});
+
+const Prediction = sequelize.define("prediction", {
+  homeScore: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayScore: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+Team.hasMany(Match, { foreignKey: "homeTeamId" });
+Team.hasMany(Match, { foreignKey: "awayTeamId" });
 Match.belongsTo(Team, { as: "homeTeam" });
 Match.belongsTo(Team, { as: "awayTeam" });
 
+Team.hasMany(Prediction, { foreignKey: "homeTeamId" });
+Team.hasMany(Prediction, { foreignKey: "awayTeamId" });
+Prediction.belongsTo(Team, { as: "homeTeam" });
+Prediction.belongsTo(Team, { as: "awayTeam" });
+
+Match.hasOne(Result);
 Result.belongsTo(Match, { as: "match" });
-Result.belongsTo(Team, { as: "homeTeam" });
-Result.belongsTo(Team, { as: "awayTeam" });
+
+Match.hasMany(Prediction);
+Prediction.belongsTo(Match, { as: "match" });
+
+User.hasMany(Prediction);
+Prediction.belongsTo(User, { as: "user" });
+
+Pool.hasMany(Prediction);
+Prediction.belongsTo(Pool, { as: "pool" });
+
+User.hasOne(Pool, { foreignKey: "ownerId" });
+Pool.belongsTo(User, { as: "owner" });
+
+// eslint-disable-next-line camelcase
+const User_Pool = sequelize.define("user_pool", {}, { timestamps: false });
+User.belongsToMany(Pool, { through: User_Pool });
+Pool.belongsToMany(User, { through: User_Pool });
 
 db[Team.name] = Team;
 db[Match.name] = Match;
 db[Result.name] = Result;
+db[User.name] = User;
+db[Prediction.name] = Prediction;
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
