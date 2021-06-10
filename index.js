5d15a8f431a1b84c69ab14366315ab00b799533e
Jon Brook <jonbrook89@gmail.com>
2021-06-10 08:23:49 +0100

    Start setting up express server

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const router = require("./router");
 const db = require("./models/index");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(router);
 
 (async () => {
-  await db.sequelize.sync({
-    force: true,
+  await db.sequelize.sync();
+  app.listen(process.env.EXPRESS_PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Listening on http://localhost:${process.env.EXPRESS_PORT}`);
   });
 })();
