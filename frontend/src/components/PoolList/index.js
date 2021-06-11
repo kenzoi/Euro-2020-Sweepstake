8322251a743528dbf6309145ffb10765952679cc
Jon Brook <jonbrook89@gmail.com>
2021-06-11 22:27:32 +0100

    SAVEPOINT

--- frontend/src/components/PoolList/index.js
+++ frontend/src/components/PoolList/index.js
@@ -1,14 +1,32 @@
-// import Leaderboard from "../Leaderboard";
-// import Prediction from "../PredictionList";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
+import Leaderboard from "../Leaderboard";
+import Prediction from "../PredictionList";
 import "./style.css";
 
 function PoolList({ data }) {
+  let { path, url } = useRouteMatch();
+
   const pools = data
     ? data.map((pool) => (
         <div className="pool-list__item" key={pool.id}>
-          <div>{pool.nanoId}</div>
-          {/* <Prediction /> */}
-          {/* <Leaderboard /> */}
+          <div>{pool.name}</div>
+          <Link to={`${url}/prediction/${pool.nanoId}`}>Prediction</Link>
+          <Link to={`${url}/leaderboard/${pool.nanoId}`}>Leaderboard</Link>
+          <Switch>
+            <Route path={`${path}/:poolId`}>
+              <Prediction />
+            </Route>
+            <Route path={`${path}/:poolId`}>
+              <Leaderboard />
+            </Route>
+          </Switch>
         </div>
       ))
     : null;
