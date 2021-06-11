01f3f199eb891f73738ce804629131af4f7c15c0
Jon Brook <jonbrook89@gmail.com>
2021-06-11 19:42:23 +0100

    add button to profile to create a new pool

--- frontend/src/components/PoolNew/index.js
+++ frontend/src/components/PoolNew/index.js
@@ -1,7 +1,17 @@
 import "./style.css";
 
+import { postPools } from "../../httpClient/axios";
+
 function PoolNew() {
-  return <div className="pool-new__container">New Pool</div>;
+  const submitHandler = async () => {
+    await postPools(1);
+  };
+
+  return (
+    <div className="pool-new__container">
+      <button onClick={submitHandler}>Add New Pool</button>
+    </div>
+  );
 }
 
 export default PoolNew;
