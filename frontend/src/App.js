b41f99958f49043d46e754010111b95e0e75277e
Jon Brook <jonbrook89@gmail.com>
2021-06-11 11:01:32 +0100

    Basic react router working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,36 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
+import Leaderboard from "./components/Leaderboard";
+import Navbar from "./components/Navbar";
+import Pool from "./components/Pool";
+import Prediction from "./components/Prediction";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <div className="app__container">
-      <Home />
-    </div>
+    <Router>
+      <div className="app__container">
+        <Navbar />
+        <Switch>
+          <Route path="/leaderboard">
+            <Leaderboard />
+          </Route>
+          <Route path="/pool">
+            <Pool />
+          </Route>
+          <Route path="/prediction">
+            <Prediction />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,7 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import "./style.css";
 
 function Navbar() {
-  return <div className="navbar__container">Navbar Page</div>;
+  return (
+    <nav className="navbar__container">
+      <Link className="navbar__item" to="/">
+        Home
+      </Link>
+      <Link className="navbar__item" to="/leaderboard">
+        Leaderboard
+      </Link>
+      <Link className="navbar__item" to="/pool">
+        Pool
+      </Link>
+      <Link className="navbar__item" to="/prediction">
+        Prediction
+      </Link>
+      <Link className="navbar__item" to="/profile">
+        Profile
+      </Link>
+    </nav>
+  );
 }
 
 export default Navbar;
