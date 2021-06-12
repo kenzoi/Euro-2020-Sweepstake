d53c2f393a7c3e142bbadac671235758d3357cfe
Jon Brook <jonbrook89@gmail.com>
2021-06-12 09:44:19 +0100

    created change and submit handler for PoolJoin

--- frontend/src/components/PoolJoin/index.js
+++ frontend/src/components/PoolJoin/index.js
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import "./style.css";
 
 function PoolJoin() {
+  const [poolText, setPoolText] = useState();
+
+  const changeHandler = (e) => {
+    const text = e.target.value;
+    setPoolText(text);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setPoolText("");
+  };
+
   return (
     <div className="pool-join__container">
       <div>Join Pool</div>
-      <input type="text"></input>
-      <button>Submit</button>
+      <form id="join" onSubmit={submitHandler}>
+        <input
+          name="pool"
+          className="pool-join__input"
+          type="text"
+          placeholder="Enter pool code here..."
+          onChange={changeHandler}
+          value={poolText}
+        ></input>
+      </form>
+      <button type="submit" form="join" value="Submit">
+        Submit
+      </button>
     </div>
   );
 }
