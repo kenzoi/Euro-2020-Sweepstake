b37aee8dd7a2ecc2a30fb4a089e5afe764c804b7
Jon Brook <jonbrook89@gmail.com>
2021-06-12 16:34:05 +0100

    destructure the specific props in PoolJoin

--- frontend/src/components/PoolJoin/index.js
+++ frontend/src/components/PoolJoin/index.js
@@ -1,18 +1,17 @@
 import "./style.css";
 
-function PoolJoin(props) {
-  console.log(props);
+function PoolJoin({ submitHandler, changeHandler, value }) {
   return (
     <div className="pool-join__container">
       <div>Join Pool</div>
-      <form id="join" onSubmit={props.submitHandler}>
+      <form id="join" onSubmit={submitHandler}>
         <input
           name="pool"
           className="pool-join__input"
           type="text"
           placeholder="Enter pool code here..."
-          onChange={props.changeHandler}
-          value={props.value}
+          onChange={changeHandler}
+          value={value}
         ></input>
       </form>
       <button type="submit" form="join" value="Submit">
