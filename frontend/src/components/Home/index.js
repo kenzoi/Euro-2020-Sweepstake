6fe2d1affa9b2792425a8efe9f35ffa53b5b4c3c
Jon Brook <jonbrook89@gmail.com>
2021-06-15 11:14:59 +0100

    Update the login page with logo and small restyle

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, TextField, Typography } from "@material-ui/core";
+import { Box, Button, TextField, Typography } from "@material-ui/core";
 import { Redirect } from "react-router-dom";
 import { login } from "../../httpClient/axios";
 import "./style.css";
@@ -22,26 +22,51 @@ function Home() {
   const loginOrRedirect = user.id ? (
     <Redirect to={`/profile/${user.id}`} />
   ) : (
-    <div>
-      <Typography variant="h5">Login</Typography>
-      <form id="login" noValidate autoComplete="off" onSubmit={loginHandler}>
-        <TextField
-          name="email"
-          label="Email"
-          variant="outlined"
-          onChange={changeHandler}
-          value={email}
+    <div className="home__container">
+      <div>
+        <Typography variant="h5">Login</Typography>
+        <form id="login" noValidate autoComplete="off" onSubmit={loginHandler}>
+          <TextField
+            className="home__emailfield"
+            name="email"
+            label="Email"
+            variant="outlined"
+            onChange={changeHandler}
+            value={email}
+          />
+        </form>
+        <Box m={1}>
+          <Button
+            type="submit"
+            form="login"
+            value="Submit"
+            variant="contained"
+            color="primary"
+          >
+            Login
+          </Button>
+        </Box>
+      </div>
+      <Box m={1} mt={5}>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/9/96/UEFA_Euro_2020_Logo.svg"
+          width="50%"
+          alt="EUFA Euro 2020 Logo"
         />
-      </form>
-      <Button
-        type="submit"
-        form="login"
-        value="Submit"
-        variant="contained"
-        color="primary"
-      >
-        Login
-      </Button>
+        <div>
+          <a
+            href="//en.wikipedia.org/wiki/File:UEFA_Euro_2020_Logo.svg"
+            title="Fair use of copyrighted material in the context of UEFA Euro 2020"
+          >
+            <Typography variant="caption">
+              By The logo is from the UEFA.,{" "}
+            </Typography>
+          </a>
+          <a href="https://en.wikipedia.org/w/index.php?curid=51705124">
+            <Typography variant="caption">Fair use</Typography>
+          </a>
+        </div>
+      </Box>
     </div>
   );
 
